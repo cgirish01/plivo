03c8c13130c813e7d9ba5c82c8f1af9798030289
cgirish01 <girishsonu2000@gmail.com>
2023-10-08 00:22:35 +0530

    added database models

--- messageService/urls.py
+++ messageService/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for messageService project.
+URL configuration for message_service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.http import JsonResponse
+from django.urls import include, path
+def handler404(request, exception):
+    return JsonResponse({"error": "Not found"}, status=404)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('messages/', include('messages.urls'))
 ]
+handler404 = handler404
--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Message(models.Model):
+    account_id = models.CharField(max_length=255)
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender_number = models.CharField(max_length=15)
+    receiver_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.sender_number} -> {self.receiver_number}"
