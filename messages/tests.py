3f062082ea343836ab7c87b46d37590f896b7553
cgirish01 <girishsonu2000@gmail.com>
2023-10-08 00:30:49 +0530

    added dependencies and test cases

--- messages/tests.py
+++ messages/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Message
 
-# Create your tests here.
+class MessageModelTestCase(TestCase):
+    def setUp(self):
+        self.message = Message.objects.create(
+            src='+1234567890',
+            dst='+0987654321',
+            text='Hello, World!'
+        )
+
+    def test_message_str(self):
+        self.assertEqual(str(self.message), 'Hello, World!')
+
+    def test_message_fields(self):
+        self.assertEqual(self.message.src, '+1234567890')
+        self.assertEqual(self.message.dst, '+0987654321')
+        self.assertEqual(self.message.text, 'Hello, World!')
